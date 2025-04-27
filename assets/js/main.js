fbbe419f72128eb138d7752ae67e1846eaf255f6
samuel <peterhunt@gmail.com>
2025-04-27 12:09:11 +0100

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+  navClose = document.getElementById("nav-close"),
+  copy_right_year = document.getElementById("copy_right_year");
 
 /* ===== MENU SHOW  =====*/
 /* Validate if constant exists */
@@ -10,6 +11,10 @@ if (navToggle) {
     navMenu.classList.add("show-menu");
   });
 }
+
+/*========= UPDATE COPY RIGHT YEAR */
+const date = new Date()
+copy_right_year.textContent=date.getFullYear()
 
 /*====== MENU HIDDEN ======*/
 /* Validate if constant exist */
@@ -54,98 +59,113 @@ const sendEmail = (e) => {
       "#contact-form",
       "FExSrjJTPL-U5wRgr"
     )
-    .then(() => {
-      // show sent message
-      contactMessage.textContent = "Message sent successfully ✅";
+    .then(
+      () => {
+        // show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
 
-      // Remove message after five seconds
-      setTimeout(() => {
-        contactMessage.textContent = "";
-      }, 5000);
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
 
-    //   Clear input fields
-    contactForm.reset()
-    }, () =>{
+        //   Clear input fields
+        contactForm.reset();
+      },
+      () => {
         // show error message
-        contactMessage.textContent = "Error sending message, please try again ❌";
-    } );
+        contactMessage.textContent =
+          "Error sending message, please try again ❌";
+      }
+    );
 };
 
 contactForm.addEventListener("submit", sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/
-const scrollUp  = () =>{
-    const scrollUp = document.getElementById('scroll-up')
-    // when the scroll is higher than 360 viewpoint add show-scroll class
-    this.scrollY >= 350 ? scrollUp.classList.add("show-scroll")
-                        : scrollUp.classList.remove("show-scroll");
-}
-window.addEventListener("scroll", scrollUp)
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // when the scroll is higher than 360 viewpoint add show-scroll class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id')
+const sections = document.querySelectorAll("section[id");
 
-const scrollActive = () =>{
-    const scrollDown = window.scrollY
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58 ,
-              sectionId = current.getAttribute('id'),
-              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
 
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-            sectionsClass.classList.add('active-link')
-        }else{
-            sectionsClass.classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== DARK LIGHT THEME ===============*/
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'ri-sun-line'
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
 
 // We validate if the user previously chose a topic
-if(selectedTheme){
-  //if the validation is fullfilled, we ask what the issue was to know if we activated or  deactivated the dark theme    
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+if (selectedTheme) {
+  //if the validation is fullfilled, we ask what the issue was to know if we activated or  deactivated the dark theme
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-} )
-
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 1700,
-    delay: 200,
-    // reset: true
-    reset: true, // Animations repeat
-})
+  origin: "top",
+  distance: "60px",
+  duration: 1700,
+  delay: 200,
+  // reset: true
+  reset: true, // Animations repeat
+});
 
-sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin: 'right'})
-sr.reveal(`.home__name, .home__info, .about__container,
+sr.reveal(`.home__perfil, .about__image, .contact__mail`, { origin: "right" });
+sr.reveal(
+  `.home__name, .home__info, .about__container,
             .section__title-1, .about__info, .contact__social,
-            .contact__data`, {origin: 'left'})
-sr.reveal(`.services__card, .projects__card`, {interval: 100})
+            .contact__data`,
+  { origin: "left" }
+);
+sr.reveal(`.services__card, .projects__card`, { interval: 100 });
